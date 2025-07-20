fad8f3ccc39f72a7de3ed7a2cd50297853e10c6c
Jamal Muradov <camal1muradov@gmail.com>
2025-07-21 00:12:19 +0400

    fix: update husky config

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -1,27 +1,3 @@
 export default {
-    parserPreset: {
-        parserOpts: {
-            headerPattern: /^\[(fix|feat|feature|test|refactor)]\[([A-Za-z0-9_-]+)] - (.+)$/,
-            headerCorrespondence: ['issue', 'title'],
-        },
-    },
-    plugins: [
-        {
-            rules: {
-                'title-pattern': ({ issue, title }) =>
-                    issue === null || title === null
-                        ? [
-                            false,
-                            'Commit message pattern should be in the form of' +
-                            ' [fix|feat|feature|test|refactor][JIRA ID] -' +
-                            ' message \n for' +
-                            ' example: [fix][CHA-876] - Fixed login cases ',
-                        ]
-                        : [true, ''],
-            },
-        },
-    ],
-    rules: {
-        'title-pattern': [2, 'always'],
-    },
-}
+    extends: ['@commitlint/config-conventional'],
+};
